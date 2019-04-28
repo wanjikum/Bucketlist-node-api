4e1db96b44f43f58661b8df27777c75b40734dac
MillicentNj <millicent.njuguna@allangray.co.za>
2019-04-29 00:03:01 +0300

    ch-add-morgan-165664331

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 import 'dotenv/config';
 
 const app = express();
@@ -9,10 +10,15 @@ const PORT = process.env.PORT || 4001;
 // Parses the text as JSON and exposes the resulting object on req.body
 app.use(bodyParser.json());
 
-// Parses the text as URL encoded data (which is how browsers tend to send 
-// form data from regular forms set to POST) and exposes the resulting object 
+// Parses the text as URL encoded data (which is how browsers tend to send
+// form data from regular forms set to POST) and exposes the resulting object
 // (containing the keys and values) on req.body.
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+
+app.get('/api/v1/auth/signup', (req, res) => {
+  res.status(200).send({ name: req.body.name, message: 'Welcome to the users route' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
