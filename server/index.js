5262fe4dca23d888fbbefa55f029bf41c5496f3e
MillicentNj <millicent.njuguna@allangray.co.za>
2019-07-18 14:12:03 +0300

    [chore] add a try catch block

--- server/index.js
+++ server/index.js
@@ -7,40 +7,43 @@ import config from './config/config';
 import router from './routes';
 
 const app = express();
+try {
+  const PORT = config.PORT || 4001;
+  mongoose.set('useNewUrlParser', true);
 
-const PORT = config.PORT || 4001;
-mongoose.set('useNewUrlParser', true);
+  mongoose.connect(config.DATABASE, { useNewUrlParser: true });
+  const db = mongoose.connection;
 
-mongoose.connect(config.DATABASE, { useNewUrlParser: true });
-const db = mongoose.connection;
+  db.on('error', () => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log('Failed to establish connection');
+    }
+  });
 
-db.on('error', () => {
+  db.once('open', () => {
+    if (process.env.NODE_ENV === 'development') {
+      app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+      });
+      console.log('Connection established');
+    }
+  });
+
+  // Parses the text as JSON and exposes the resulting object on req.body
+  app.use(bodyParser.json());
+
+  // Parses the text as URL encoded data (which is how browsers tend to send
+  // form data from regular forms set to POST) and exposes the resulting object
+  // (containing the keys and values) on req.body.
+  app.use(bodyParser.urlencoded({ extended: true }));
+
   if (process.env.NODE_ENV === 'development') {
-    console.log('Failed to establish connection');
+    app.use(morgan('combined'));
   }
-});
 
-db.once('open', () => {
-  if (process.env.NODE_ENV === 'development') {
-    app.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT}`);
-    });
-    console.log('Connection established');
-  }
-});
-
-// Parses the text as JSON and exposes the resulting object on req.body
-app.use(bodyParser.json());
-
-// Parses the text as URL encoded data (which is how browsers tend to send
-// form data from regular forms set to POST) and exposes the resulting object
-// (containing the keys and values) on req.body.
-app.use(bodyParser.urlencoded({ extended: true }));
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('combined'));
+  app.use('/api/v1', router);
+} catch (err) {
+  console.log('error>>>>>', err);
 }
-
-app.use('/api/v1', router);
 
 export default app;
